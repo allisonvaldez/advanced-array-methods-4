4e1fce84bd9ee9a47e2240f3afb9fd04477c83e5
Allison Valdez <ajsnipes@usfca.edu>
2022-12-23 13:19:00 -0500

    finished with questions

--- reduce.js
+++ reduce.js
@@ -1,15 +1,27 @@
 /*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+QUESTIONS:
+1. #2 why order vowel.indexOf(lowerCase) and not lowerCase.indexOf(vowel)
+2. #3 walk through logic not understanding what matches to what/logic
+3. #3 why not returning [] instead arr
+*/
+
+/*
+1. Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce((acc, nextObj) => {
+        acc.push(nextObj[key]);
+        return acc;
+    },[]);
+}
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+2. Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -19,10 +31,23 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
-
+function vowelCount(str) {
+    let vowel = "aeiou";
+    return str.split("").reduce((acc, next) => {
+        let lowerCase = next.toLowerCase();
+        if (vowel.indexOf(lowerCase) !== -1) {
+            if (acc[lowerCase]) {
+                acc[lowerCase] += 1;
+            } else {
+                acc[lowerCase] = 1;
+             }
+        }
+        return acc;
+    },{});
+}
+ 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+3. Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -36,10 +61,15 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce((acc, next, index) => {
+        acc[index][key] = value;
+        return acc;
+    }, arr);
+}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+4. Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
     
@@ -60,4 +90,13 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce((acc, next) => {
+        if (callback(next)) {
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    },[[],[]]);
+}
